47b2243c67b535cb551c79efe46ec19c38789590
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-09 13:18:06 -0300

    feat: initial pages and routes setup

---
+++ frontend/src/components/Chat/inde.tsx
@@ -0,0 +1,11 @@
+const Chat = () => {
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input type="text" placeholder="Mensagem" />
+      <button>Enviar</button>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ frontend/src/components/Join/index.tsx
@@ -0,0 +1,11 @@
+const Join = () => {
+  return (
+    <div>
+      <h1>Join</h1>
+      <input type="text" placeholder="Nome" />
+      <button>Entrar</button>
+    </div>
+  );
+};
+
+export default Join;
